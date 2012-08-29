6faf8fe9fd96e1f083496adbd3a303d4ef846d63
mauriciosilva <mauricio.silva@gmail.com>
2012-08-28 21:09:03 -0700

    added alias for nocorrect tree

--- plugins/mauo/mauo.plugin.zsh
+++ plugins/mauo/mauo.plugin.zsh
@@ -2,6 +2,8 @@
 alias pryr="pry -r ./config/environment -r rails/console/app -r rails/console/helpers"
 
 alias v='vagrant'
+
+alias tree='nocorrect tree'
 ## shortcuts
 cotweet=/Users/msilva/Projects/et/cotweet
 

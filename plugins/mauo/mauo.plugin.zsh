e7d2f5fb3bbd09ea6aae8c7057465e147a489a5e
mauriciosilva <mauricio.silva@gmail.com>
2012-11-30 18:31:41 -0800

    removed pry alias

--- plugins/mauo/mauo.plugin.zsh
+++ plugins/mauo/mauo.plugin.zsh
@@ -1,9 +1,26 @@
 ## aliases
-alias pryr="pry -r ./config/environment -r rails/console/app -r rails/console/helpers"
+alias zshconf='vi ~/.oh-my-zsh/plugins/mauo/mauo.plugin.zsh'
+
+alias c='bundle exec rails c'
+
+alias pryr="bundle exec pry -r ./config/environment -r rails/console/app -r rails/console/helpers"
 
 alias v='vagrant'
 
 alias tree='nocorrect tree'
+alias Ack='nocorrect Ack'
+
+alias be='bundle exec'
+
+alias gc='git checkout'
+alias gs='git status'
+alias gsp='git smart-pull'
+alias gsl='git smart-log'
+
+
+#git branch by time
+alias gbrt='ruby ~/src/gbrt/gbrt.rb'
 ## shortcuts
 cotweet=/Users/msilva/Projects/et/cotweet
 
+#RPROMPT='[%* on %D]' # prompt for right side of scree

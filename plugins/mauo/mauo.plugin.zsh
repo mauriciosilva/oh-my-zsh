0b98ee0ba6762e22ef4aa8ae8541e8ac8845a5ab
mauriciosilva <mauricio.silva@gmail.com>
2013-04-08 14:59:32 -0700

    minor alias updates

--- plugins/mauo/mauo.plugin.zsh
+++ plugins/mauo/mauo.plugin.zsh
@@ -7,8 +7,20 @@ alias pryr="bundle exec pry -r ./config/environment -r rails/console/app -r rail
 
 alias v='vagrant'
 
+alias vi="vim"
+alias start='be thin start -p 8080'
+
 alias tree='nocorrect tree'
 alias Ack='nocorrect Ack'
+alias ack='nocorrect Ack'
+
+alias chef='nocorrect chef'
+alias knife='nocorrect knife'
+
+alias cookbook='nocorrect cookbook'
+alias environment='nocrroect environment'
+
+alias role='nocorrect role'
 
 alias be='bundle exec'
 
@@ -24,3 +36,7 @@ alias gbrt='ruby ~/src/gbrt/gbrt.rb'
 cotweet=/Users/msilva/Projects/et/cotweet
 
 #RPROMPT='[%* on %D]' # prompt for right side of scree
+alias t='tree -L 1'
+
+## clear iterm
+alias cleariterm='sudo rm -rf /var/log/asl/*.asl'
